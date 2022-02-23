88a70c6225293f1e89ca1eff960639058e9f6efd
Angeloamenta <angeloamenta7@gmail.com>
2022-02-23 14:34:15 +0100

    aggiunto bootstrap ed iniziato form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -2,6 +2,19 @@
 
 @section('content')
 
-<div>ciao</div>
+<div class="container">
+    <div class="row">
+      <div class="col">
+        <form>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+      </div>
+    </div>
+  </div>
 
 @endsection
