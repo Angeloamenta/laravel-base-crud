1d3fe614d5d88f32f91b7158b6c758c504850864
Angeloamenta <angeloamenta7@gmail.com>
2022-02-25 15:22:11 +0100

    aggiunta validate anche a store

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -11,26 +11,45 @@
             <div class="mb-3">
               <label for="title" class="form-label">title</label>
               <input type="text" class="form-control" id="title" name="title">
+              @error('title')
+              <div class="alert alert-danger">{{ $message }}</div>
+              @enderror
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">description</label>
                 <input class="form-control" id="description" name="description">
+                @error('description')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+                n
               </div>
               <div class="mb-3">
                 <label for="thumb" class="form-label">thumb</label>
                 <input type="text" class="form-control" id="thumb" name="thumb">
+                @error('thumb')
+              <div class="alert alert-danger">{{ $message }}</div>
+              @enderror
               </div>
               <div class="mb-3">
                 <label for="series" class="form-label">series</label>
                 <input type="text" class="form-control" id="series" name="series">
+                @error('series')
+              <div class="alert alert-danger">{{ $message }}</div>
+              @enderror
               </div>
               <div class="mb-3">
                 <label for="author" class="form-label">author</label>
                 <input type="text" class="form-control" id="author" name="author">
+                @error('author')
+              <div class="alert alert-danger">{{ $message }}</div>
+              @enderror
               </div>
               <div class="mb-3">
                 <label for="price" class="form-label">price</label>
                 <input type="number" class="form-control" id="price" name="price">
+                @error('price')
+              <div class="alert alert-danger">{{ $message }}</div>
+              @enderror
               </div>
             <button type="submit" class="btn btn-primary">Submit</button>
           </form>
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -19,22 +19,37 @@
             <div class="mb-3">
                 <label for="description" class="form-label">description</label>
                 <input class="form-control" id="description" name="description" value="{{$comic->description}}">
+                @error('description')
+              <div class="alert alert-danger">{{ $message }}</div>
+              @enderror
               </div>
               <div class="mb-3">
                 <label for="thumb" class="form-label">thumb</label>
                 <input type="text" class="form-control" id="thumb" name="thumb" value="{{$comic->thumb}}">
+                @error('thumb')
+              <div class="alert alert-danger">{{ $message }}</div>
+              @enderror
               </div>
               <div class="mb-3">
                 <label for="series" class="form-label">series</label>
                 <input type="text" class="form-control" id="series" name="series" value="{{$comic->series}}">
+                @error('series')
+              <div class="alert alert-danger">{{ $message }}</div>
+              @enderror
               </div>
               <div class="mb-3">
                 <label for="author" class="form-label">author</label>
                 <input type="text" class="form-control" id="author" name="author" value="{{$comic->author}}">
+                @error('author')
+              <div class="alert alert-danger">{{ $message }}</div>
+              @enderror
               </div>
               <div class="mb-3">
                 <label for="price" class="form-label">price</label>
                 <input type="number" class="form-control" id="price" name="price" value="{{$comic->price}}">
+                @error('price')
+              <div class="alert alert-danger">{{ $message }}</div>
+              @enderror
               </div>
             <button type="submit" class="btn btn-primary">Salva</button>
           </form>
